aa464d18eb6c3b6a2645e111bde42994d869fa82
nagibator6k <vanaiox@mail.ru>
2024-05-17 19:10:09 +0300

    1

--- django_test1/main/views.py
+++ django_test1/main/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from forms import regestration
-from models import user
-
-
 def main(request):
     return render(request,'main/main2.html')
 
-def about(request):
-    return HttpResponse("<h4>about<h4>")
-
 def reg(request):
     return render(request,'main/registration.html')
+
+def cm(request):
+    return render(request,'main/coctails_make.html')
+
+def search_of_ingrid(request):
+    return render(request, "main/search_of_ingrid.html")
+
+def prof(request):
+    return render(request, "main/my_profile.html")
+
+def log(request):
+    return render(request, "main/login.html")
