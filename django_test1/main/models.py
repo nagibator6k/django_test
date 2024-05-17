bc2a069a83e8a1c060c1a76afe8fdbe39aee6f51
nagibator6k <vanaiox@mail.ru>
2024-05-17 19:11:39 +0300

    2

--- django_test1/main/models.py
+++ django_test1/main/models.py
@@ -5,3 +5,5 @@ class user (models.Model):
     name = models.CharField(max_length=25, blank=False)
     password = models.CharField(max_length=25, blank=False)
 
+    def __str__(self):
+        return self.name
