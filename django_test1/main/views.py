ff925a202f41ab782f3cd4ab5b99058abcb5148b
ByFor4 <by.for4@gmail.com>
2024-05-17 14:28:31 +0300

    fix_reg

--- django_test1/main/views.py
+++ django_test1/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def index(request):
+def main(request):
     return render(request,'main/main2.html')
 
 def about(request):
     return HttpResponse("<h4>about<h4>")
+
+def reg(request):
+    return render(request,'main/registration.html')
